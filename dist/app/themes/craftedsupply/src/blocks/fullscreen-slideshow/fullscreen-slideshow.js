39d1b0ab9b3b0b0a42f230d13be2ab2d9651a674
Christopher Silich <chris.silich@gmail.com>
2024-07-30 00:35:22 -0400

    Slideshow and hamburger button fixes

--- dist/app/themes/craftedsupply/src/blocks/fullscreen-slideshow/fullscreen-slideshow.js
+++ dist/app/themes/craftedsupply/src/blocks/fullscreen-slideshow/fullscreen-slideshow.js
@@ -4,15 +4,29 @@ import Swiper from 'swiper'
 import { Autoplay, EffectFade } from 'swiper/modules'
 
 export default (el) => {
-	const $slider = el.querySelector('.swiper')
+	const $slider = el.querySelector('.swiper'),
+		between = parseInt(el.dataset.slideshowBetween) || 3000,
+		transition = parseInt(el.dataset.slideshowTransition) || 1500
+
+	console.log('between', between)
+	console.log('transition', transition)
 
 	new Swiper($slider, {
 		modules: [Autoplay, EffectFade],
 		direction: 'horizontal',
 		loop: true,
-		speed: 1500,
-		spaceBetween: 30,
-		autoplay: { delay: 3000, disableOnInteraction: true },
+		speed: transition,
+		autoplay: { delay: between, disableOnInteraction: true },
 		effect: 'fade',
+	})
+
+	// Make the hamburger nav change color appropriately
+	window.addEventListener('scroll', () => {
+		const coords = el.getBoundingClientRect()
+		if (coords.top > 120 || coords.bottom < 120) {
+			document.querySelector('html').classList.remove('-white-menu-mode')
+		} else {
+			document.querySelector('html').classList.add('-white-menu-mode')
+		}
 	})
 }
